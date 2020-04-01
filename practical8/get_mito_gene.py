22eadcab47c50b51f34441cf0a728f4654cba623
Xiaotian Li <3190111060@intl.zju.edu.cn>
2020-04-01 12:57:11 +0800

    practical8

--- practical8/get_mito_gene.py
+++ practical8/get_mito_gene.py
@@ -7,23 +7,29 @@ Created on Wed Apr  1 09:53:22 2020
 
 Name = []
 gene = []
+leng = []
 s=''
 count = 0
+length = 0
 flag = False
 xfile = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
 for line in xfile:
     if line.startswith ('>'):
         if s!='':
             gene.append(s)
+            leng.append(length)
         s = line[1:8]
+        length = 0
         Name.append(s)
         count = count + 1
         s = ''
     else:
         s = s + line 
+        length = length + len(line)
 gene.extend(s)
 
 seq = input ('please input the gene name')
+l = int(input('please input the gene length'))
 for i in range (count):
     if seq == Name[i]:
         print(gene[i])
@@ -32,16 +38,17 @@ for i in range (count):
 if flag == False:
     print ('No such sequence here')
     
+fout = open('mito_gene.fa','w')
+line1 = Name[i] + '   ' + str(leng[i]) + '\n'
+line2 = gene[i]
+fout.write(line1)
+fout.write(line2)
+
 try:
 	xfile = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
 finally:
 	if xfile:
 		xfile.close()
-fout = open('mito_gene.fa','w')
-line1 = Name[i]
-line2 = gene[i]
-fout.write(line1)
-fout.write(line2)
 fout.close()
 
   
